dc7b6bb2b9de8313369e79d7a0552f60fbce53f4
dslifecoach <dslifecoach@example.com>
2025-10-27 18:09:56 +0800

    feat: switch to HashRouter for GitHub Pages compatibility

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
 import './App.css';
@@ -9,7 +9,7 @@ import App from './App'; // Import App
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/my-music-universe">
+    <HashRouter>
       <App /> {/* Render App */}
     </BrowserRouter>
   </React.StrictMode>
